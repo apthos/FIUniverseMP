5519f66fcc5466922fb36813348ad6f37d60b53d
apthos <david00lara@gmail.com>
2022-03-09 18:45:16 -0500

    Added network player spawn

---
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    private GameObject spawnedPlayerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawnedPlayerPrefab);
+    }
+}
