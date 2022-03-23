9a4d14cb6e89eb6bc5a37b0aa2f5651725cc2c47
apthos <david00lara@gmail.com>
2022-03-23 19:41:48 -0400

    Added customization assets

---
+++ Assets/Scripts/AvatarTypes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarTypes
+{
+    public static readonly string HEAD = "HEAD";
+    public static readonly string SKIN = "SKIN";
+    public static readonly string FACE = "FACE";
+    public static readonly string HAIR = "HAIR";
+    public static readonly string HAIR_COLOR = "HAIR_COLOR";
+}
---
+++ Assets/Scripts/CustomizationMenu.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizationMenu : MonoBehaviour
+{
+    // GameObject to be customized for the menu
+    public GameObject avatar;
+
+    // Active customization GameObjects
+    public GameObject head;
+    public GameObject face;
+    public GameObject hairStyle;
+    public GameObject hairColor;
+
+    // GameObjects and Materials for customization
+    public GameObject[] heads;
+    public Material[] skinTones;
+    public GameObject[] facesMale;
+    public GameObject[] facesFemale;
+    public GameObject[] hairStylesMale;
+    public GameObject[] hairStylesFemale;
+    public Material[] hairColors;
+
+    // Player Input
+    public int headSelection;
+    public int skinToneSelection;
+    public int faceSelection;
+    public int hairStyleSelection;
+    public int hairColorSelection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPreferences();
+        UpdateAvatar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // Updates head shape. Also, face & hair style change since they are each fitted to a specific head shape
+    public void UpdateHead()
+    {
+        Destroy(head);
+        head = Instantiate(heads[headSelection], avatar.transform);
+        UpdateFace();
+        UpdateHair();
+    }
+
+    // Updates skin tone of head component
+    public void UpdateSkinTone()
+    {
+        head.GetComponent<Renderer>().material = skinTones[skinToneSelection];
+    }
+
+    // Updates face & eyebrow color
+    public void UpdateFace()
+    {
+        if (headSelection == 0)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    // Updates hair style & color
+    public void UpdateHair()
+    {
+        if (headSelection == 0)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void UpdateAvatar()
+    {
+        UpdateHead();
+        UpdateSkinTone();
+    }
+
+    public void ChangePreference(string characteristic, int value)
+    {
+        PlayerPrefs.SetInt(characteristic, value);
+    }
+
+    public void LoadPreferences()
+    {
+        headSelection = PlayerPrefs.GetInt(AvatarTypes.HEAD, 0);
+        skinToneSelection = PlayerPrefs.GetInt(AvatarTypes.SKIN, 0);
+        faceSelection = PlayerPrefs.GetInt(AvatarTypes.FACE, 0);
+        hairStyleSelection = PlayerPrefs.GetInt(AvatarTypes.HAIR, 0);
+        hairColorSelection = PlayerPrefs.GetInt(AvatarTypes.HAIR_COLOR, 0);
+    }
+
+    // Method in case customization will be saved onto a database per account
+    public void SavePreferences()
+    {
+        PlayerPrefs.Save();
+    }
+}
