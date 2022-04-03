94bb6542359eb4a7d0171984183803b34900ca5d
apthos <david00lara@gmail.com>
2022-04-03 12:35:18 -0400

    Added XR/Desktop control switching

---
+++ Assets/Scripts/XREnable.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class XREnable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IsXREnabled())
+        {
+            GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+            mainCamera.GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.Both;
+            mainCamera.GetComponent<UnityEngine.InputSystem.XR.TrackedPoseDriver>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsXREnabled()
+    {
+        var xrInputSubsystems = new List<XRInputSubsystem>();
+        SubsystemManager.GetInstances<XRInputSubsystem>(xrInputSubsystems);
+
+        foreach (var subsystem in xrInputSubsystems)
+        {
+            if (subsystem.running)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
