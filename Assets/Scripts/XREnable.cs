523d8b56c883ab22eff22f2f32128359d9b14456
apthos <david00lara@gmail.com>
2022-04-07 19:38:04 -0400

    Fixed XR Enable script

--- Assets/Scripts/XREnable.cs
+++ Assets/Scripts/XREnable.cs
@@ -8,18 +8,31 @@ public class XREnable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        IsXREnabled();
+        /*
         if (!IsXREnabled())
         {
             GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
             mainCamera.GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.None;
             mainCamera.GetComponent<UnityEngine.InputSystem.XR.TrackedPoseDriver>().enabled = false;
         }
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        if (IsXREnabled())
+        {
+            mainCamera.GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.Both;
+            mainCamera.GetComponent<UnityEngine.InputSystem.XR.TrackedPoseDriver>().enabled = true;
+        }
+        else
+        {
+            mainCamera.GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.None;
+            mainCamera.GetComponent<UnityEngine.InputSystem.XR.TrackedPoseDriver>().enabled = false;
+        }
     }
 
     public bool IsXREnabled()
